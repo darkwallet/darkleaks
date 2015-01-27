af3f4488cd6dbff29ff7e57f95514d86e7b7d6e7
809894ac <809894ac@email.com>
2015-01-26 23:47:00 -0600

    add more buttons, quit btn

--- app/main.py
+++ app/main.py
@@ -4,19 +4,52 @@ include_path = os.path.dirname(os.path.realpath(__file__)) + "/../python/"
 sys.path.append(include_path)
 # All sub-modules below this line will now be able to import darkleaks module!
 import darkleaks
-from PyQt4 import QtGui
 
+import shutil
+import hashlib
+from PyQt4 import QtGui, QtCore
+
+def checksum(filename):
+    f = open(filename)
+    return hashlib.sha1(f.read()).hexdigest()
+
+def showDialog(parent):
+    fname = QtGui.QFileDialog.getOpenFileName(parent, 'Open file', '/home')
+
+def getChunks(source_filename, number_chunks, chunks_directory):
+  setup_testdir(chunks_directory)
+  print "Digest of source file:", checksum(source_filename)
+
+  actual_chunks = darkleaks.start(source_filename, chunks_directory, number_chunks)
+  print "Created", actual_chunks, "chunks."
 
 def main(args):
     print "hello!", args
 
     app = QtGui.QApplication(sys.argv)
 
-    w = QtGui.QWidget()
-    w.resize(250, 150)
-    w.move(300, 300)
-    w.setWindowTitle('HelloWorld')
-    w.show()
+    self = QtGui.QWidget()
+
+    openButton = QtGui.QPushButton('Choose leaking document', self)
+    openButton.move(200, 10)
+
+    generateButton = QtGui.QPushButton('Generate Chunks', self)
+    generateButton.setCheckable(True)
+    generateButton.move(10, 10)
+
+    #openFile = QtGui.QAction('Open', self)
+    #openFile.triggered.connect(showDialog)
+
+    openButton.clicked.connect(lambda : showDialog(self))
+    generateButton.clicked[bool].connect(getChunks)
+
+    qbtn = QtGui.QPushButton('Quit', self)
+    qbtn.clicked.connect(QtCore.QCoreApplication.instance().quit)
+    qbtn.move(50, 50)       
+    
+    self.setGeometry(300, 300, 250, 150)
+    self.setWindowTitle('HelloWorld')
+    self.show()
     sys.exit(app.exec_())
 
 if __name__ == "__main__":
